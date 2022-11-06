417b40a5f6617fed14d08b16718100c7a718fdf1
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-11-06 17:58:12 +0100

    part4-ticketing project data

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,6 +1,8 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.mapper.TaskMapper;
+import com.cydeo.repository.TaskRepository;
 import com.cydeo.service.TaskService;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +11,13 @@ import java.util.List;
 @Service
 public class TaskServiceImpl implements TaskService {
 
+    private final TaskRepository taskRepository;
+    private final TaskMapper taskMapper;
 
+    public TaskServiceImpl(TaskRepository taskRepository, TaskMapper taskMapper) {
+        this.taskRepository = taskRepository;
+        this.taskMapper = taskMapper;
+    }
 
     @Override
     public List<TaskDTO> listAllTasks() {
